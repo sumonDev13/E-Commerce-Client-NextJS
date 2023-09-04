66d282ddba6e5491da37cb9f43461ae88be32d08
sumonDev13 <sumonmondal1530765@gmail.com>
2023-09-04 22:45:40 +0600

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default page
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <>
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Welcome to the e-shop
+      </h1>
+    </div>
+  </>
+  )
+}
